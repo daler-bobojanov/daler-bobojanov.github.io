7396dd16d3cf82b8653689b207d1dc4aea372890
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-03-13 15:17:23 -0400

    more updates to index, style, and js. Also added gameBoardImage into assets

--- RPGame-Rock, Paper, Scissors/js/script.js
+++ RPGame-Rock, Paper, Scissors/js/script.js
@@ -1,28 +1,24 @@
-console.log('Hello Daler');
-
 // Game Pseudocode
 
 // <------ On landing page, Modal with "Instructions" and "Start" button ------>
+const $gameBoard = document.body.querySelector('.gameBoard');
 
+const $player1 = document.body.querySelector('.playerOne');
+const $player2 = document.body.querySelector('playerTwo');
 
+const $rock = document.body.querySelector('.rock');
+const $paper = document.body.querySelector('.paper');
+const $scissors = document.body.querySelector('.scissors');
 
-
+const $gameBoardWidth = $gameBoard.offsetWidth;
 
 // <------ Implement "swap" method for characters to be able to switch 'weapons ------>
-
-
-
 
 
 // <------ Implement "collision detection" method for Win/Lose state ----->
 
 
-
-
-
-
 // <------ Another Modal to announce which Player won. And "Play again" button ----->
-
 
 
 // <------ Creat eventListeners for key controls/press for both characters ------>
